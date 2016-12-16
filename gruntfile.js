944303ff810867f4efef8728bcc9cfaf79d0b4b2
Brian Brennan <me@brianbrennan.com>
2016-12-16 14:15:06 -0500

    bundling up a build

--- gruntfile.js
+++ gruntfile.js
@@ -4,23 +4,40 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
+        //typescript compilation options
         ts: {
             build: {
                 src: ['./app/**/*.ts'],
+                out: './dist/app.js',
                 options: {
                     target: 'es5',
-                    module: 'commonjs',
+                    module: 'system',
                     moduleResolution: 'node',
                     sourceMap: false,
                     emitDecoratorMetadata: false,
                     experimentalDecorators: true,
                     lib: ['es2015', 'dom'],
                     noImplicitAny: true,
-                    suppressImplicitAnyIndexErrors: true
+                    suppressImplicitAnyIndexErrors: true,
+                    forceConsistentCasingInFileNames: true,
+                    pretty: true,
+                    fast: 'never'
                 }
             }
+        },
+        //sass compilation options
+        sass: {
+            options: {
+                sourceMap: false
+            },
+            dist: {
+                files: {
+                    './dist/styles.css': './app/globals/assets/scss/main.scss'
+                }
+            }
+
         }
     });
 
-    grunt.registerTask('build', ['ts:build']);
+    grunt.registerTask('build', ['ts:build', 'sass']);
 };
